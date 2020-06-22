52a2fba154b558a8a2544d1de2adbec17460b743
Dante Nolasco <dnolascodante@gmail.com>
2020-06-22 10:30:48 -0300

    add localstorage helper

--- view/src/hooks/helpers.jsx
+++ view/src/hooks/helpers.jsx
@@ -19,3 +19,12 @@ export const deepDiff = (object, base) => {
   }
   return changes(object, base);
 };
+
+export const asyncLocalStorage = {
+  setItem: (key, value) => {
+    return Promise.resolve().then(localStorage.setItem(key, value));
+  },
+  getItem: (key) => {
+    return Promise.resolve().then(localStorage.getItem(key));
+  },
+};
