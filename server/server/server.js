c003f704fbde7ee3cc1fd95a27918aea40f5f71e
Dante Nolasco <dnolascodante@gmail.com>
2020-07-03 12:40:49 -0300

    ev http or https setup

--- server/server/server.js
+++ server/server/server.js
@@ -3,19 +3,28 @@ require('dotenv').config();
 const loopback = require('loopback');
 const boot = require('loopback-boot');
 const https = require('https');
+const http = require('http');
 const fileUpload = require('express-fileupload');
 
 const certs = require('./ssl/certs');
 const app = (module.exports = loopback());
 app.use(fileUpload());
 
-app.start = async function () {
+app.start = async function() {
   const options = await certs();
 
-  const server = https.createServer(options, app);
+  let server, uri;
+  if (process.env.SSL === 'false') {
+    uri = 'http';
+    server = http.createServer(app);
+  } else {
+    uri = 'https';
+    server = https.createServer(options, app);
+  }
+
   // start the web server
-  return server.listen(app.get('port'), function () {
-    const baseUrl = 'https://' + app.get('host') + ':' + app.get('port');
+  return server.listen(app.get('port'), function() {
+    const baseUrl = uri + '://' + app.get('host') + ':' + app.get('port');
     app.emit('started', baseUrl);
     console.log('LoopBack server listening @ %s%s', baseUrl, '/');
 
@@ -28,7 +37,7 @@ app.start = async function () {
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function (err) {
+boot(app, __dirname, function(err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
