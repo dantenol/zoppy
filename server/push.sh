9abaaca781fd4e809a737e0f516e0db91b642d65
Dante Nolasco <dnolascodante@gmail.com>
2020-08-09 19:45:59 -0300

    replace version

--- server/push.sh
+++ server/push.sh
@@ -1,5 +1,5 @@
-sed -i "s/latest/$v/g" "../.github/workflows/s3.yml"
+sed -i "s/\"latest\"/$v/g" "../.github/workflows/s3.yml"
 git add .
 git commit -m 'replace version'
 git push
-sed -i "s/$v/latest/g" "../.github/workflows/s3.yml"
+sed -i "s/$v/\"latest\"/g" "../.github/workflows/s3.yml"
