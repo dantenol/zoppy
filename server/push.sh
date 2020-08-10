46d73fdb9bd203470ed1fc21c064001909fbd2e8
Dante Nolasco <dnolascodante@gmail.com>
2020-08-09 22:00:22 -0300

    update save script

--- server/push.sh
+++ server/push.sh
@@ -1,8 +1,12 @@
 v=$(awk -F"v" 'BEGIN{OFS=FS} $1==""{$2=$2+1}1' LATEST)
 
-sed -i "s/\"latest\"/$v/g" "../.github/workflows/s3.yml"
+sed -i "s/\"latest\"/\"$v\"/g" "../.github/workflows/s3.yml"
 echo $v
-git add .
+git add ../.github/workflows/s3.yml
 git commit -m 'replace version'
 git push
-sed -i "s/$v/\"latest\"/g" "../.github/workflows/s3.yml"
+sed -i "s/\"$v\"/\"latest\"/g" "../.github/workflows/s3.yml"
+git add ../.github/workflows/s3.yml
+git commit -m 'update latest'
+git commit -m 'replace version'
+git push
