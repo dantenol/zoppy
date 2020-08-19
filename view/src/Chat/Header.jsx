7daa2b1f607cebb9e06390284cc99c5afa7fd0d9
Dante Nolasco <dnolascodante@gmail.com>
2020-08-19 19:39:57 -0300

    fix showing details

--- view/src/Chat/Header.jsx
+++ view/src/Chat/Header.jsx
@@ -5,16 +5,18 @@ import classes from "./index.module.css";
 import back from "../assets/images/back.svg";
 
 const Header = ({ data, handleBack, handleToggleDetails }) => (
-  <div className={classes.header} onClick={handleToggleDetails}>
+  <div className={classes.header}>
     <button
       onClickCapture={handleBack}
       className={classNames(classes.arrow, "mobile-only")}
     >
       <img src={back} alt="back" />
     </button>
-    <img className={classes.profilePic} src={data.profilePic} alt="Foto" />
-    <div className={classes.name}>
-      <div className={classes.name}>{data.displayName}</div>
+    <div className={classes.headerContent} onClick={handleToggleDetails} >
+      <img className={classes.profilePic} src={data.profilePic} alt="Foto" />
+      <div className={classes.name}>
+        <div className={classes.name}>{data.displayName}</div>
+      </div>
     </div>
   </div>
 );
