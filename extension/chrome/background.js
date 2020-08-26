dd88043908b67424695b40a8ed940463260b23d3
Dante Nolasco <dnolascodante@gmail.com>
2020-08-26 18:53:55 -0300

    update extension

--- extension/chrome/background.js
+++ extension/chrome/background.js
@@ -1,21 +1,28 @@
 function saveUrl(url) {
-  chrome.storage.local.set({ url }, function () {
-    console.log("Value is set to " + url);
+  const subdomain = url.match(/:\/\/(.+?).zoppy/g)[0];
+  const domain = "https://" + subdomain + ".zoppy.app"
+  // const domain = "http://localhost:3000"
+  chrome.storage.local.set({ url: domain }, function () {
+    console.log("Value is set to " + domain);
   });
 }
 
 function redirect(id, url) {
   chrome.storage.local.get(["url"], function (result) {
-    let number = "";
+    let myUrl = result.url;
     if (url.startsWith("https://wa.me")) {
       const nbr = /wa.me\/(\d+)/g.exec(url);
-      number = nbr[1];
+      myUrl += "?phone=" + nbr;
     } else if (url.startsWith("https://api.whatsa")) {
       const nbr = /phone=(\d+)/g.exec(url);
-      number = nbr[1];
+      const text = /text=([a-zA-z0-9%]+)/g.exec(url);
+      console.log(nbr, text);
+      myUrl += "?" + nbr[0];
+      if (text) {
+        myUrl += "&" + text[0];
+      }
     }
-    chrome.tabs.update(id, { url: result.url + number }, () => {
-    });
+    chrome.tabs.update(id, { url: myUrl }, () => {});
   });
 }
 
