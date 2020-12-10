bbe0278b7a3436552e8cd1bc5a856879703087b0
Dante Nolasco <dnolascodante@gmail.com>
2020-12-10 07:56:09 -0300

    updade connector launch

--- static/launch/index.js
+++ static/launch/index.js
@@ -1,1 +1,5 @@
-const connector = "https://localhost:3001/api/";
+let connector = window.location.origin + "/api/";
+
+if (window.location.origin.includes("localhost")) {
+  connector = "https://localhost:3001/api/"
+}
