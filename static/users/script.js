0fb12aface318b2da7d5ef361ed9f3c5e1b6ab89
Dante Nolasco <dnolascodante@gmail.com>
2020-12-10 11:12:52 -0300

    create user screen

--- static/users/script.js
+++ static/users/script.js
@@ -9,3 +9,37 @@ function generatePassword() {
   }
   randomPassword.value = random;
 }
+
+async function createUser() {
+  const path = document.getElementById("url").value;
+  const fullName = document.getElementById("name").value;
+  const username = document.getElementById("user").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    const url = `https://${path}.zoppy.app/api/agents`;
+    const options = {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ fullName, username, password }),
+    };
+    const res = await fetch(url, options);
+    const data = await res.json();
+    alert("Usuário criado!");
+    console.log(data);
+    return false;
+  } catch (e) {
+    alert("Algo deu errado!");
+    console.log(e);
+  }
+}
+
+window.onload = () => {
+  function handleForm(event) {
+    event.preventDefault();
+  }
+  const form = document.getElementById("myForm");
+  form.addEventListener("submit", handleForm);
+};
