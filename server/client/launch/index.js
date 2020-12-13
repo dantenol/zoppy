24ff1296d1bfd3b386f18b05172482ec1a5c6082
Dante Nolasco <dnolascodante@gmail.com>
2020-12-13 16:21:27 -0300

    replace version

--- server/client/launch/index.js
+++ server/client/launch/index.js
@@ -2,4 +2,5 @@ let connector = window.location.origin + "/api/";
 
 if (window.location.origin.includes("localhost")) {
   connector = "https://localhost:3001/api/"
+  // connector = "https://vmh1.zoppy.app/api/"
 }
