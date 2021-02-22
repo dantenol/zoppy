3b4d8c4cc56b11b84f4dde6e0a1900c4552d8eeb
Dante Nolasco <dnolascodante@gmail.com>
2021-02-22 13:27:43 -0300

    fix quote box to show images

--- view/src/Chat/ChatBox/Bubble/QuoteBox.jsx
+++ view/src/Chat/ChatBox/Bubble/QuoteBox.jsx
@@ -4,13 +4,21 @@ import classNames from 'classnames'
 
 import classes from "./index.module.css";
 
+const BodyContent = ({data}) => {
+  if (data && data.length > 200 && !data.includes(' ')) {
+    return <img alt="imagem respondida" src={`data:image/jpg;base64,${data}`} />
+  } else {
+    return <span>{data}</span>
+  }
+}
+
 const QuoteBox = ({data}) => {
-  const {body, sender, mine, timestamp} = data
+  const {body, sender, mine} = data
   return (
     <div className={classNames(classes.quote, classes[mine])}>
       <span className={classes.from}>{sender}</span>
       <br/>
-      <span>{body}</span>
+      <BodyContent data={body} />
     </div>
   )
 }
